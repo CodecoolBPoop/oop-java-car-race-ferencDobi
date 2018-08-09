9a9dd2897e113d5308bdfd7428887b412e047e4f
ferencDobi <ferencdbi@gmail.com>
2018-08-09 16:47:59 +0200

    refactor vehicle to an abstract class

--- src/com/codecool/Vehicle.java
+++ src/com/codecool/Vehicle.java
@@ -1,24 +1,9 @@
 package com.codecool;
 
-public class Vehicle {
+public abstract class Vehicle {
 
     private String name;
     private int normalSpeed;
-    /*
-     TODO: Car: the normal speed of the car. Set to a random number in the constructor between 80-110km/h.
-     TODO: Motorcycle: speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
-     TODO: speed: 100km/h. 5% chance of breaking down for 2 hours.
-    */
-
-    public String name() {
-        /*  TODO: Make a list from the words here: http://www.fantasynamegenerators.com/car-names.php
-                and pick 2 randomly for each instance.
-            TODO: Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
-            TODO: Truck drivers are boring. They call all their trucks a random number between 0 and 1000.
-         */
-        return "";
-    }
-
     private int distanceTraveled; // holds the current distance traveled.
 
     /**
@@ -26,23 +11,29 @@ public class Vehicle {
      *     If there is a broken down Truck on the track, then limit the max speed of vehicles to 75 km/h.
      * @param race
      */
-    public void moveForAnHour(Race race) {
-        // TODO
-    }
+    public abstract void moveForAnHour(Race race);
 
-    public int getNormalSpeed() {
+    int getNormalSpeed() {
         return normalSpeed;
     }
 
-    public void setNormalSpeed(int normalSpeed) {
-        this.normalSpeed = normalSpeed;
+    void setNormalSpeed(int normalSpeed) {
+        if (this.normalSpeed == 0) this.normalSpeed = normalSpeed;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    void setName(String name) {
+        if (this.name == null) this.name = name;
+    }
+
+    int getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    void updateDistanceTraveled(int distance) {
+        distanceTraveled += distance;
     }
 }
