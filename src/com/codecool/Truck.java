537ec52564c55d0fc46e0b0686a7d98b8d4ba7bd
ferencDobi <ferencdbi@gmail.com>
2018-08-09 16:40:47 +0200

    truck constructor implement

--- src/com/codecool/Truck.java
+++ src/com/codecool/Truck.java
@@ -1,9 +1,18 @@
 package com.codecool;
 
-public class Truck {
-    Truck() {
-        // TODO
-    }
+import static com.codecool.RandomGen.*;
+
+public class Truck extends Vehicle {
 
     private int breakdownTurnsLeft; // holds how long its still broken down.
+
+    public Truck() {
+        setName(String.valueOf(randomNumber(0, 1000)));
+        setNormalSpeed(100);
+    }
+
+    @Override
+    public void moveForAnHour(Race race) {
+        // TODO 5% chance of breaking down for 2 hours.
+    }
 }
